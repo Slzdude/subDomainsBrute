e83e67c416f699311565c3d9cb16ac572c0da9f7
Skactor <sk4ct0r@gmail.com>
2019-12-14 10:15:22 +0800

    black

--- lib/common.py
+++ lib/common.py
@@ -8,25 +8,25 @@ console_width = getTerminalSize()[0] - 2
 
 
 def is_intranet(ip):
-    ret = ip.split('.')
+    ret = ip.split(".")
     if len(ret) != 4:
         return True
-    if ret[0] == '10':
+    if ret[0] == "10":
         return True
-    if ret[0] == '172' and 16 <= int(ret[1]) <= 31:
+    if ret[0] == "172" and 16 <= int(ret[1]) <= 31:
         return True
-    if ret[0] == '192' and ret[1] == '168':
+    if ret[0] == "192" and ret[1] == "168":
         return True
     return False
 
 
 def print_msg(msg=None, left_align=True, line_feed=False):
     if left_align:
-        sys.stdout.write('\r' + msg + ' ' * (console_width - len(msg)))
+        sys.stdout.write("\r" + msg + " " * (console_width - len(msg)))
     else:  # right align
-        sys.stdout.write('\r' + ' ' * (console_width - len(msg)) + msg)
+        sys.stdout.write("\r" + " " * (console_width - len(msg)) + msg)
     if line_feed:
-        sys.stdout.write('\n')
+        sys.stdout.write("\n")
     sys.stdout.flush()
 
 
@@ -35,42 +35,46 @@ def test_server(server, dns_servers):
     resolver.lifetime = resolver.timeout = 5.0
     try:
         resolver.nameservers = [server]
-        answers = resolver.query('public-dns-a.baidu.com')    # an existed domain
-        if answers[0].address != '180.76.76.76':
-            raise Exception('Incorrect DNS response')
+        answers = resolver.query("public-dns-a.baidu.com")  # an existed domain
+        if answers[0].address != "180.76.76.76":
+            raise Exception("Incorrect DNS response")
         try:
-            resolver.query('test.bad.dns.lijiejie.com')    # non-existed domain
-            with open('bad_dns_servers.txt', 'a') as f:
-                f.write(server + '\n')
-            print_msg('[+] Bad DNS Server found %s' % server)
+            resolver.query("test.bad.dns.lijiejie.com")  # non-existed domain
+            with open("bad_dns_servers.txt", "a") as f:
+                f.write(server + "\n")
+            print_msg("[+] Bad DNS Server found %s" % server)
         except Exception as e:
             dns_servers.append(server)
-        print_msg('[+] Server %s < OK >   Found %s' % (server.ljust(16), len(dns_servers)))
+        print_msg(
+            "[+] Server %s < OK >   Found %s" % (server.ljust(16), len(dns_servers))
+        )
     except Exception as e:
-        print_msg('[+] Server %s <Fail>   Found %s' % (server.ljust(16), len(dns_servers)))
+        print_msg(
+            "[+] Server %s <Fail>   Found %s" % (server.ljust(16), len(dns_servers))
+        )
 
 
 def load_dns_servers():
-    print_msg('[+] Validate DNS servers', line_feed=True)
+    print_msg("[+] Validate DNS servers", line_feed=True)
     dns_servers = []
     pool = Pool(5)
-    for server in open('dict/dns_servers.txt').readlines():
+    for server in open("dict/dns_servers.txt").readlines():
         server = server.strip()
-        if server and not server.startswith('#'):
+        if server and not server.startswith("#"):
             pool.apply_async(test_server, (server, dns_servers))
     pool.join()
 
     server_count = len(dns_servers)
-    print_msg('\n[+] %s DNS Servers found' % server_count, line_feed=True)
+    print_msg("\n[+] %s DNS Servers found" % server_count, line_feed=True)
     if server_count == 0:
-        print_msg('[ERROR] No valid DNS Server !', line_feed=True)
+        print_msg("[ERROR] No valid DNS Server !", line_feed=True)
         sys.exit(-1)
     return dns_servers
 
 
 def load_next_sub(options):
     next_subs = []
-    _file = 'dict/next_sub_full.txt' if options.full_scan else 'dict/next_sub.txt'
+    _file = "dict/next_sub_full.txt" if options.full_scan else "dict/next_sub.txt"
     with open(_file) as f:
         for line in f:
             sub = line.strip()
@@ -78,15 +82,15 @@ def load_next_sub(options):
                 tmp_set = {sub}
                 while tmp_set:
                     item = tmp_set.pop()
-                    if item.find('{alphnum}') >= 0:
-                        for _letter in 'abcdefghijklmnopqrstuvwxyz0123456789':
-                            tmp_set.add(item.replace('{alphnum}', _letter, 1))
-                    elif item.find('{alpha}') >= 0:
-                        for _letter in 'abcdefghijklmnopqrstuvwxyz':
-                            tmp_set.add(item.replace('{alpha}', _letter, 1))
-                    elif item.find('{num}') >= 0:
-                        for _letter in '0123456789':
-                            tmp_set.add(item.replace('{num}', _letter, 1))
+                    if item.find("{alphnum}") >= 0:
+                        for _letter in "abcdefghijklmnopqrstuvwxyz0123456789":
+                            tmp_set.add(item.replace("{alphnum}", _letter, 1))
+                    elif item.find("{alpha}") >= 0:
+                        for _letter in "abcdefghijklmnopqrstuvwxyz":
+                            tmp_set.add(item.replace("{alpha}", _letter, 1))
+                    elif item.find("{num}") >= 0:
+                        for _letter in "0123456789":
+                            tmp_set.add(item.replace("{num}", _letter, 1))
                     elif item not in next_subs:
                         next_subs.append(item)
     return next_subs
@@ -96,9 +100,9 @@ def get_out_file_name(target, options):
     if options.output:
         outfile = options.output
     else:
-        _name = os.path.basename(options.file).replace('subnames', '')
-        if _name != '.txt':
-            _name = '_' + _name
+        _name = os.path.basename(options.file).replace("subnames", "")
+        if _name != ".txt":
+            _name = "_" + _name
         outfile = target + _name
     return outfile
 
--- lib/consle_width.py
+++ lib/consle_width.py
@@ -4,19 +4,24 @@
 """
 import os
 
-__all__ = ['getTerminalSize']
+__all__ = ["getTerminalSize"]
 
 
 def getTerminalSize():
     import platform
+
     current_os = platform.system()
     tuple_xy = None
-    if current_os == 'Windows':
+    if current_os == "Windows":
         tuple_xy = _getTerminalSize_windows()
         if tuple_xy is None:
             tuple_xy = _getTerminalSize_tput()
             # needed for window's python in cygwin's xterm!
-    if current_os == 'Linux' or current_os == 'Darwin' or current_os.startswith('CYGWIN'):
+    if (
+        current_os == "Linux"
+        or current_os == "Darwin"
+        or current_os.startswith("CYGWIN")
+    ):
         tuple_xy = _getTerminalSize_linux()
     if tuple_xy is None:
         tuple_xy = (80, 25)  # default value
@@ -39,8 +44,20 @@ def _getTerminalSize_windows():
         return None
     if res:
         import struct
-        (bufx, bufy, curx, cury, wattr,
-         left, top, right, bottom, maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+
+        (
+            bufx,
+            bufy,
+            curx,
+            cury,
+            wattr,
+            left,
+            top,
+            right,
+            bottom,
+            maxx,
+            maxy,
+        ) = struct.unpack("hhhhHhhhhhh", csbi.raw)
         sizex = right - left + 1
         sizey = bottom - top + 1
         return sizex, sizey
@@ -53,10 +70,15 @@ def _getTerminalSize_tput():
     # src: http://stackoverflow.com/questions/263890/how-do-i-find-the-width-height-of-a-terminal-window
     try:
         import subprocess
-        proc = subprocess.Popen(["tput", "cols"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        proc = subprocess.Popen(
+            ["tput", "cols"], stdin=subprocess.PIPE, stdout=subprocess.PIPE
+        )
         output = proc.communicate(input=None)
         cols = int(output[0])
-        proc = subprocess.Popen(["tput", "lines"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        proc = subprocess.Popen(
+            ["tput", "lines"], stdin=subprocess.PIPE, stdout=subprocess.PIPE
+        )
         output = proc.communicate(input=None)
         rows = int(output[0])
         return (cols, rows)
@@ -68,7 +90,8 @@ def _getTerminalSize_linux():
     def ioctl_GWINSZ(fd):
         try:
             import fcntl, termios, struct, os
-            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))
+
+            cr = struct.unpack("hh", fcntl.ioctl(fd, termios.TIOCGWINSZ, "1234"))
         except:
             return None
         return cr
@@ -84,7 +107,7 @@ def _getTerminalSize_linux():
     if not cr:
         try:
             env = os.environ
-            cr = (env['LINES'], env['COLUMNS'])
+            cr = (env["LINES"], env["COLUMNS"])
         except:
             return None
     return int(cr[1]), int(cr[0])
@@ -92,4 +115,4 @@ def _getTerminalSize_linux():
 
 if __name__ == "__main__":
     sizex, sizey = getTerminalSize()
-    print('width =', sizex, 'height =', sizey)
+    print("width =", sizex, "height =", sizey)
